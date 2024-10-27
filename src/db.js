da461554b2f84371bf21dfb47f4462d0e1055f1e
Vinicius Nascimento <viniciusncrocha@gmail.com>
2024-10-27 19:04:36 -0300

    alteraçao para vercel

--- src/db.js
+++ src/db.js
@@ -7,16 +7,13 @@ const dbHost = process.env.DB_HOST;
 const dbPassword = process.env.DB_PASSWORD;
 const dbPort = process.env.DB_PORT || 5432; // porta padrão do PostgreSQL
 
-// const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-//   //passar os dados para o sequelize
-//   dialect: "postgres", // informar que estamos usando PostgreSQL
-//   host: dbHost, // o host do banco
-//   port: dbPort, // porta do PostgreSQL
-//   logging: false, // desabilitar logs do Sequelize (opcional)
-// });
-
-const sequelize = new Sequelize('postgres://backendinter:Inter2024@@5.161.105.90:5432/startup_inter',{
-  dialectModule: require('pg')
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  //passar os dados para o sequelize
+  dialect: "postgres", // informar que estamos usando PostgreSQL
+  dialectModule: pg, // I've added this.
+  host: dbHost, // o host do banco
+  port: dbPort, // porta do PostgreSQL
+  logging: false, // desabilitar logs do Sequelize (opcional)
 });
 
 // sequelize.authenticate()
